99fdcbd936fd764578e29d64832521e2b4a17cf4
tiking76 <716yosiki1999@gmail.com>
2020-01-31 04:39:00 +0900

    add mlmodel file

--- convert.py
+++ convert.py
@@ -1,10 +1,18 @@
 """this is a test code """
 import coremltools
+import numpy as np
+import cv2
+from keras.models import model_from_json
+from keras.preprocessing.image import load_img, img_to_array
 
-path = 'MNIST.h5'
+MODEL_ARC_PATH = "finetuning.h5"
 
-coreml_model = coremltools.converters.keras.convert(path,
-                                                    input_names='image',
-                                                    output_names='image',
-                                                    class_labels='label.txt')
-coreml_model.save('number.mlmodel')
+# JSONファイルからモデルのアーキテクチャを得る
+model_arc_str = open(MODEL_ARC_PATH).read()
+model = model_from_json(model_arc_str)
+
+# モデル構成の確認
+model.summary()
+
+coreml_model = coremltools.converters.keras.convert('finetuning.h5')
+coreml_model.save('vtuber.mlmodel')
